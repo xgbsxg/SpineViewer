4fad5842e5ceaccbfdc3b84f3b35b486427dd210
xgbsxg <xiao_gu1113@163.com>
2026-07-27 08:41:45 +0800

    Update PreferenceManager.java

--- app/src/main/java/com/spineviewer/utils/PreferenceManager.java
+++ app/src/main/java/com/spineviewer/utils/PreferenceManager.java
@@ -5,10 +5,11 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import androidx.annotation.Nullable;
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.spineviewer.spine.SpineFileInfo;
+import com.spineviewer.spine.SpineVersion;
 
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,16 +37,80 @@ public class PreferenceManager {
     }
 
     public void saveFileList(List<SpineFileInfo> fileList) {
-        String json = gson.toJson(fileList);
+        JsonArray array = new JsonArray();
+        for (SpineFileInfo info : fileList) {
+            JsonObject obj = new JsonObject();
+            obj.addProperty("skeletonUri", info.skeletonUri.toString());
+            if (info.atlasUri != null) {
+                obj.addProperty("atlasUri", info.atlasUri.toString());
+            }
+            obj.addProperty("name", info.name);
+            obj.addProperty("isBinary", info.isBinary);
+            if (info.detectedVersion != null) {
+                obj.addProperty("detectedVersion", info.detectedVersion.name());
+            }
+            if (info.selectedVersion != null) {
+                obj.addProperty("selectedVersion", info.selectedVersion.name());
+            }
+            if (info.rawVersionString != null) {
+                obj.addProperty("rawVersionString", info.rawVersionString);
+            }
+            obj.addProperty("fileSizeBytes", info.fileSizeBytes);
+
+            JsonArray siblings = new JsonArray();
+            for (Uri uri : info.siblingUris) {
+                siblings.add(uri.toString());
+            }
+            obj.add("siblingUris", siblings);
+
+            array.add(obj);
+        }
+        String json = gson.toJson(array);
         prefs.edit().putString(KEY_FILE_LIST, json).apply();
     }
 
     public List<SpineFileInfo> getFileList() {
         String json = prefs.getString(KEY_FILE_LIST, null);
         if (json == null) return new ArrayList<>();
-        Type type = new TypeToken<List<SpineFileInfo>>(){}.getType();
-        List<SpineFileInfo> list = gson.fromJson(json, type);
-        return list != null ? list : new ArrayList<>();
+
+        List<SpineFileInfo> list = new ArrayList<>();
+        try {
+            JsonArray array = gson.fromJson(json, JsonArray.class);
+            for (int i = 0; i < array.size(); i++) {
+                JsonObject obj = array.get(i).getAsJsonObject();
+                Uri skeletonUri = Uri.parse(obj.get("skeletonUri").getAsString());
+                Uri atlasUri = obj.has("atlasUri") ? Uri.parse(obj.get("atlasUri").getAsString()) : null;
+                String name = obj.get("name").getAsString();
+                boolean isBinary = obj.get("isBinary").getAsBoolean();
+
+                SpineFileInfo info = new SpineFileInfo(skeletonUri, atlasUri, name, isBinary);
+
+                if (obj.has("detectedVersion")) {
+                    String v = obj.get("detectedVersion").getAsString();
+                    info.detectedVersion = SpineVersion.valueOf(v);
+                }
+                if (obj.has("selectedVersion")) {
+                    String v = obj.get("selectedVersion").getAsString();
+                    info.selectedVersion = SpineVersion.valueOf(v);
+                }
+                if (obj.has("rawVersionString")) {
+                    info.rawVersionString = obj.get("rawVersionString").getAsString();
+                }
+                info.fileSizeBytes = obj.get("fileSizeBytes").getAsLong();
+
+                if (obj.has("siblingUris")) {
+                    JsonArray siblings = obj.get("siblingUris").getAsJsonArray();
+                    for (int j = 0; j < siblings.size(); j++) {
+                        info.siblingUris.add(Uri.parse(siblings.get(j).getAsString()));
+                    }
+                }
+
+                list.add(info);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 
     public void clearFileList() {
