84e8e120dda6d02874e761c144b6c368af528bc3
xgbsxg <xiao_gu1113@163.com>
2026-07-27 09:21:39 +0800

    Update SettingsActivity.java

--- app/src/main/java/com/spineviewer/ui/SettingsActivity.java
+++ app/src/main/java/com/spineviewer/ui/SettingsActivity.java
@@ -21,7 +21,7 @@ public class SettingsActivity extends AppCompatActivity {
     private Button btnSelectFolder;
     private Button btnClearFolder;
 
-    private ActivityResultLauncher<String> folderPickerLauncher;
+    private ActivityResultLauncher<Void> folderPickerLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
